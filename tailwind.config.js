fa8f8da733135293890150de1f0c3bcdb37cbed8
Josh <josh@sf-iron.com>
2023-05-08 17:01:10 -0700

    Added LoadingSpinner animation. Added loading time and AI response disclaimer.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         bounce:
           "bounce 0.5s alternate cubic-bezier(0.95, 0.05, 0.795, 0.035) infinite",
         slideUpCubiBezier: "slideUp 1s cubic-bezier(0.165, 0.84, 0.44, 1)",
+        spinner: "spinner 1s linear infinite",
       },
       keyframes: {
         bounce: {
@@ -21,6 +22,11 @@ module.exports = {
         slideUp: {
           from: { transform: "translateY(100%)" },
           to: { transform: "translateY(0)" },
+        },
+        spinner: {
+          to: {
+            transform: "rotate(360deg)",
+          },
         },
       },
     },
