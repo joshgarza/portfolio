d0b9aed6611499837538bd23007bdff19b708019
Josh <josh@sf-iron.com>
2024-05-01 20:42:24 -0500

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Navbar />
           {children}
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
